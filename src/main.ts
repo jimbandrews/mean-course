e6e8cc561d22090b1db507db51894af41a35b7cd
James Andrews <jaandre2@cisco.com>
2022-05-12 11:14:17 -0400

    added comments on what this main.ts does

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// This is the first file executed.
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -8,5 +10,6 @@ if (environment.production) {
   enableProdMode();
 }
 
+// This line starts the application, which is referenced by the AppModule
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

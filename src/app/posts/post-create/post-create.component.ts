564847238abb04a54cc33eaa413e9000e480ebfb
James Andrews <jaandre2@cisco.com>
2022-05-12 11:30:00 -0400

    added comments about how a component class is constructed

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+// this decorator turns the class below into something that Angular understands
+// selector creates the html tag that you will use for this component
+// templateUrl and styleUrls are the relative paths of the html and css files for the component
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
 
+// Creating component class
 export class PostCreateComponent implements OnInit {
 
   constructor() { }

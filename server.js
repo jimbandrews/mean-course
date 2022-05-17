2e50f3cca6af8283549457a81bb44654cce90d62
James Andrews <jaandre2@cisco.com>
2022-05-17 15:20:21 -0400

    added comments to new server.js code

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// still importing app and http
 const app = require("./backend/app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
+// makes sure port number is valid when trying to set up a port
 const normalizePort = val => {
   var port = parseInt(val, 10);
 
@@ -18,6 +20,7 @@ const normalizePort = val => {
   return false;
 };
 
+// check the type of error occurred, respond, and exit node server
 const onError = error => {
   if (error.syscall !== "listen") {
     throw error;
@@ -37,15 +40,18 @@ const onError = error => {
   }
 };
 
+// logging that we are listening for incoming requests
 const onListening = () => {
   const addr = server.address();
   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
   debug("Listening on " + bind);
 };
 
+// still setting up port
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
+// set up server, activate it, and register listeners above
 const server = http.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);

bce94fa9bca1702d1d595e8efd408f46340d9702
James Andrews <jaandre2@cisco.com>
2022-05-17 15:51:24 -0400

    created an api route that sends back dummy data as a json

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware')
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+// the new first arg is the path that we have to send a request to
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'osidf80932',
+      title: 'First server-side post',
+      content: 'This is coming from a server.'
+    },
+    {
+      id: 'ljknvkd435',
+      title: 'Second server-side post',
+      content: 'This is coming from a server!'
+    }
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts
+  });
 });
 
 module.exports = app;

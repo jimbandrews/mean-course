8c584725c8ad19207a59afb87f4f832522583c13
James Andrews <jaandre2@cisco.com>
2022-05-17 09:38:49 -0400

    added methods to get posts and add a new one

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -8,4 +8,14 @@ export class PostsService {
   private posts: Post[] = []
 
   constructor() { }
+
+  getPosts() {
+    // this is a best practice when copying reference data types, like arrays
+    return [...this.posts];
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = {title: title, content: content};
+    this.posts.push(post)
+  }
 }

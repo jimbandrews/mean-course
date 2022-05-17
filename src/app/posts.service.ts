b585054bfc5b6e487cca70635e3178967e5a4521
James Andrews <jaandre2@cisco.com>
2022-05-17 10:52:55 -0400

    implemented Subject to act as an eventemitter

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
 import { Post } from './posts/post.model';
 
 @Injectable({
@@ -6,6 +8,7 @@ import { Post } from './posts/post.model';
 })
 export class PostsService {
   private posts: Post[] = []
+  private postsUpdated = new Subject<Post[]>();
 
   constructor() { }
 
@@ -17,5 +20,6 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post)
+    this.postsUpdated.next([...this.posts])
   }
 }

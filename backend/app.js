3bd1d236f33acb232957c456af1b0ab641f05ca9
James Andrews <jaandre2@cisco.com>
2022-05-18 14:18:03 -0400

    added endpoint to handle deleting post from database; had to change get request from app.use to app.get for deleting to work

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,7 @@ app.post("/api/posts", (req, res, next) => {
 
 // the new first arg is the path that we have to send a request to
 // we can call either app.use or app.get here
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
   Post.find()
     .then(documents => {
       res.status(200).json({
@@ -56,6 +56,13 @@ app.use('/api/posts', (req, res, next) => {
         posts: documents
       });
     });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: "Post deleted!"});
+  });
 });
 
 module.exports = app;

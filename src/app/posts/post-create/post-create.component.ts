b88482c3533cbec6fea8b31dbfb2f6298a997a7f
James Andrews <jaandre2@cisco.com>
2022-05-18 17:02:37 -0400

    implemented OnInit and added private properties to detect if the component is being used to create a new post or edit an existing one

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { PostsService } from 'src/app/posts.service';
+import { Post } from '../post.model';
 
 // this decorator turns the class below into something that Angular understands
 // selector creates the html tag that you will use for this component
@@ -12,13 +14,26 @@ import { PostsService } from 'src/app/posts.service';
 })
 
 // Creating component class
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredContent = '';
   enteredTitle = '';
+  private mode = 'create';
+  private postId: string
+  private post: Post
 
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = null
+      }
+    });
   }
 
   onAddPost(form: NgForm) {
